4a783e257a726113e8831c9f310d19775258a4ac
Max <yurchenko.maksym@gmail.com>
2026-02-18 13:27:42 +0000

    V6

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -24,6 +24,7 @@ export default function NotePreviewClient({ noteId }: Props) {
     queryKey: ["note", noteId],
     queryFn: () => fetchNoteById(noteId),
     enabled: Boolean(noteId),
+    refetchOnMount: false,
   });
 
   return (
